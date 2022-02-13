c59ac7d4ccd2e2ecea7b3d791a471c8a9dfaa452
Zimmendra C.H <99347087+IT21265242Zimmendra@users.noreply.github.com>
2022-02-13 13:40:23 +0000

    completed tute 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,14 @@
 #include <stdio.h>
 
 int main() {
-  
+  int sub1,sub2;
+  float average;
+  printf("marks of the subject one:"); // ask for input
+  scanf("%d",&sub1);
+  printf("marks of the subject one:"); // ask for input
+  scanf("%d",&sub2);
+  average = (sub1 + sub2)/2;     // Calculations
+  printf("total mark is: %.2f",average); //display average
   return 0;
 }
 
