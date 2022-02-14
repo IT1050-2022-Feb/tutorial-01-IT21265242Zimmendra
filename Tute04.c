77f3cb7429ce90564425ffae83626f4669e2b443
Zimmendra C.H <99347087+IT21265242Zimmendra@users.noreply.github.com>
2022-02-14 05:30:07 +0000

    tute 4 completed

--- Tute04.c
+++ Tute04.c
@@ -5,15 +5,31 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
-
+int multiply(int number1,int number2); // function intialization  
+int maximum(int number1,int number2); // function intialization  
+int minimum(int number1,int number2); // function intialization  
 int main() {
    int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
-   return 0;
+   printf("Enter a value for no 1 : "); // get inputs from users 
+   scanf("%d", &no1); 
+   printf("Enter a value for no 2 : "); // get inputs from users 
+   scanf("%d", &no2); 
+   printf("%d ", minimum(no1, no2)); // calling the function
+   printf("%d ", maximum(no1, no2)); // calling the function
+   printf("%d ", multiply(no1, no2)); // calling the function
+   return 0; 
 }
+int multiply(int number1,int number2) //Function Declarations
+{
+  return number1*number2;
+}
+int maximum(int number1,int number2) //Function Declarations
+{
+  return number1+number2;
+  
+}
+int minimum(int number1,int number2)  //Function Declarations
+{
+return number1-number2;
+}
+
